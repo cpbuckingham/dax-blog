fcdeab3423e9ec9aeed6037890d09506f8cb939b
Dax <limeside@gmail.com>
2016-07-28 09:17:41 -0500

    Edit knexfile, should actually fucking work now. :rabbit:

--- knexfile.js
+++ knexfile.js
@@ -29,9 +29,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection:{
-      database:process.env.DATABASE_URL
-    },
+    connection:process.env.DATABASE_URL,
     pool:{
       min:0,
       max:10
