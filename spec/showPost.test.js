9007b4ec0b962a28281c24b9081ed2b94b67569f
Dax <limeside@gmail.com>
2016-08-02 21:01:14 -0500

    Edit spec. :rabbit:

--- spec/showPost.test.js
+++ spec/showPost.test.js
@@ -2,7 +2,18 @@ var supertest = require('supertest'),
     should    = require('should'),
     server    = supertest.agent('http://localhost:3000');
 
+
+
+
 describe('#Index Route', () => {
+  before(){
+    // Start server?
+  }
+
+  after(){
+    // End server.
+  }
+
   it('Should return home page.', done => {
     server
       .get('/')
@@ -15,6 +26,16 @@ describe('#Index Route', () => {
 });
 
 describe('#Post Routes', () => {
+
+  before( () => {
+    // Run knex migrations.
+    // Populate the database, run seed.
+  });
+
+  after( () => {
+    // Roll back migration.
+  });
+
   it('Should return array of posts.', done => {
     server
       .get('/api/posts')
