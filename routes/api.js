c9af2cf67a8ca73c67ce1d3eb101c52fa1d15aa9
Dax <limeside@gmail.com>
2016-08-02 22:43:09 -0500

    Add test for api/post post. :rabbit:

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,17 @@ router.route('/posts')
     })
   })
 
+  //TODO: Refactor to method of post.
+  .post((req, res) => {
+    new Post({
+      blog_title:req.body.blog_title,
+      blog_body:req.body.blog_body,
+    }).save()
+      .then(results => {
+        res.json(results.toJSON());
+      })
+  })
+
 
 router.route('/posts/:post_id')
   .get((req, res) => {
--- test/showPost.test.js
+++ test/showPost.test.js
@@ -5,59 +5,84 @@ var knex = require('../db/knex');
 var should = require('should');
 
 
+describe('Route Test', () => {
+  describe('/', () => {
 
-describe('#Index Route', () => {
-
-  it('Should return home page.', done => {
-    request
-      .get('/')
-      .expect('Content-type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        done();
-      });
+    it('Should return home page.', done => {
+      request
+        .get('/')
+        .expect('Content-type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          done();
+        });
+    });
   });
-});
 
-describe('#Post Routes', () => {
+  describe('/api/posts', () => {
 
-  before(done => {
-    knex.migrate.latest()
-    .then( results => {
-      knex.seed.run()
-      .then (results => {
+    before(done => {
+      knex.migrate.latest()
+      .then( results => {
+        knex.seed.run()
+        .then (results => {
+          done();
+        })
+      })
+    })
+
+    after(done => {
+      knex.migrate.rollback()
+      .then( results => {
         done();
       })
     })
-  })
 
-  after(done => {
-    knex.migrate.rollback()
-    .then( results => {
-      done();
+    it('Should return array of posts.', done => {
+      request
+        .get('/api/posts')
+        .expect(200)
+        .end((err, res) => {
+          var post = res.body;
+          expect(res.body.length).to.eq(3);
+          (post[0]).should.have.property('blog_title', 'Seed Title 1');
+          (post[0]).should.have.property('blog_body', 'Seed Post 1');
+          done();
+        })
     })
-  })
 
-  it('Should return array of posts.', done => {
-    request
-      .get('/api/posts')
-      .expect(200)
-      .end((err, res) => {
-        var post = res.body;
-        expect(res.body.length).to.eq(3);
-        (post[0]).should.have.property('blog_title', 'Seed Title 1');
-        (post[0]).should.have.property('blog_body', 'Seed Post 1');
-        done();
-      })
-  })
+    it('Should return a single post.', done => {
+      request
+        .get('/api/posts/1')
+        .expect('Content-type',/json/)
+        .expect(200)
+        .end((err, res) => {
+          var post = res.body;
+          (post).should.have.property('blog_title', 'Seed Title 1');
+          (post).should.have.property('blog_body', 'Seed Post 1');
+          done();
+        });
+    });
 
-  it('Should return a single post.', done => {
-    request
-      .get('/api/posts/1')
-      .expect('Content-type',/json/)
-      .expect(200)
-      .end((err, res) => {
-        done();
-      });
+    it('Should post a new blog.', done => {
+      var newPost = {blog_title:'Test', blog_body:'Test'}
+
+      request
+        .post('/api/posts')
+        .expect(200)
+        .send(newPost)
+        .end((err, res) => {
+          request
+            .get(`/api/posts/${res.body}`)
+            .expect(200)
+            .end((err, res) => {
+              var post = res.body;
+
+              (post).should.have.property('blog_title', 'Test');
+              (post).should.have.property('blog_post', 'Test');
+              done();
+            });
+        });
+    });
   });
 });
