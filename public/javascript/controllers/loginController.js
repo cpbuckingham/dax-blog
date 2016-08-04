0fb220120caf0265acb4d8d99f94f9ecf73c03d6
Dax <limeside@gmail.com>
2016-08-04 09:17:42 -0500

    Add loginController and added to main app. :rabbit:

--- public/javascript/controllers/loginController.js
+++ public/javascript/controllers/loginController.js
@@ -1,22 +1,26 @@
 (function(){
   angular
-    .module('login', ['ngRoute'])
+    .module('login', [])
     .config(routeConfig)
     .controller('LoginController', LoginController)
 
-  routeConfig.$inject = ['$routeProvider,$locationProvider']
   LoginController.$inject = []
 
   function routeConfig($routeProvider, $locationProvider){
     $routeProvider
       .when('/auth/login',{
-        templateUrl:'/views/login',
+        templateUrl:'/views/blog/login',
         controller:'LoginController',
         controllerAs:'login'
       })
+
+    $locationProvider.html5Mode({
+      enabled:true,
+      requireBase:false
+    })
   }
 
   function LoginController(){
     this.greeting = 'Hello.'
   }
-})()
+})();
