76e29cf1228f30a0e51476447bfd55b8c15f42d2
Dax <limeside@gmail.com>
2016-08-03 22:50:02 -0500

    Add user auth strats and login, need to make tests. :rabbit:

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 var express = require('express'),
     router  = express.Router();
 
-router.route('/signin')
+router.route('/login')
   .get((req, res) => {
     // Do the thing, renders signin page, but should be handled in angular?
   })
 
-  .post((req, res) => {
-    // Do the user credentials.
-  })
+  .post(passport.authenticate('local', {
+    failureRedirect: '/auth/login'
+  }), (req, res) => {
+    res.redirect('/');
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,6 +20,24 @@ app.use(bodyParser.json())
 
 
 // --- Passport Strategy --- //
+passport.use(new LocalStrategy({
+	usernameField: 'username',
+	passwordField: 'password',
+	session: false
+}, (username, password, done) => {
+	// Check id of user, retrieve row in users table.
+	Users().where('username', username)
+		.first()
+		.then( user => {
+			// compareSync the user's hashed password.
+			if (user && bcrypt.compareSync(password, user.password)){
+				// On match, return confirmation of session.
+				return done(null, user);
+			}
+			// Otherwise, return no session, redirect.
+			return done(null, false);
+		})
+}));
 
 // --- Routing --- //
 
